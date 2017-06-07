e184164d8ec82ea5002b34bee16674f4e8ff1164
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-07 20:36:03 +0800

    Updated dependency.

--- mtch.support.js
+++ mtch.support.js
@@ -35,6 +35,9 @@
               			"module": "mtch",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/mtch.git",
               			"test": "mtch-test.js",
               			"global": true
